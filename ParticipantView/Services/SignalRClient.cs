26a356de810fb91f9bd0ecf83879826c3d258367
matthi-g <matthias.gassner@student.uibk.ac.at>
2024-11-19 23:54:17 +0100

    Initialize SignalR connection

---
+++ ParticipantView/Services/SignalRClient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ParticipantView.Services;
+
+public class SignalRClient
+{
+    private HubConnection _connection;
+    
+    public event Action<string, string>? OnMessageReceived;
+    
+    public async Task ConnectAsync()
+    {
+        try
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5000/tournamentHub")
+                .Build();
+        
+            _connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                OnMessageReceived?.Invoke(user, message);
+                Console.WriteLine($"Received message: {user}: {message}");
+            });
+        
+            await _connection.StartAsync();
+            Console.WriteLine("SignalR connected!");
+        } 
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error connecting to SignalR hub: {ex.Message}");
+        }
+    }
+    
+    public async Task SendMessage(string user, string message)
+    {
+        await _connection.InvokeAsync("BroadcastMessage", user, message);
+    }
+    
+}
---
+++ Shared/TournamentHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Shared;
+
+public class TournamentHub : Hub
+{
+    public async Task BroadcastMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
